af5ef641a4eba4bfb9d71554eff5a61dcb780b9c
Thiagomr58 <thiagomr58@outlook.com>
2017-07-11 11:59:39 -0300

    primeiro commit

---
+++ src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
@@ -0,0 +1,34 @@
+package com.algaworks.curso.jpa2.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+
+@Entity
+public class Fabricante {
+
+	// posso declarar o id em cima do atributo ou em cima do get referente a ele.
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long codigo;
+	private String nome;
+	
+	/* esta é outra maneira de declarar o incremento do atributo
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	*/
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
